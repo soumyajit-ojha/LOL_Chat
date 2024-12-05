f65f1ce88c1a1eebfff71c758dad645fe54a67f5
soumyajit-ojha <soumyajitojhag2@gmail.com>
2024-12-05 13:31:27 +0530

    included account app urls.

--- backend/pochat/urls.py
+++ backend/pochat/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from personal.views import *
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('personal.urls')),
+    path('', include('account.urls')),
 
 ]
