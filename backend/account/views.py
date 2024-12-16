ed990b1b8015f51898adc6bf273b7e55fb7317e1
soumyajit-ojha <soumyajitojhag2@gmail.com>
2024-12-16 15:18:22 +0530

    views added: account_view and account_search_view

--- backend/account/views.py
+++ backend/account/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.http import HttpResponse
 from account.forms import RegistrationForm, LoginForm
+from . models import Account
+from django.conf import settings
+from django.db.models import Q
+
 
 def registration_view(request, *args, **kwargs):
     user = request.user
@@ -51,3 +55,62 @@ def login_view(request):
         context['login_form'] = LoginForm()
 
     return render(request, 'account/login.html', context)
+
+
+# User account and its modification
+def account_view(request, *args, **kwargs):
+
+    context = {}
+    user_id = kwargs.get("user_id")
+    print(f"User ID {user_id}")
+    try:
+        account = Account.objects.get(pk = user_id)
+    except:
+        HttpResponse("<h3 class = text-center> Something went wrong.</h3>")
+    if account:
+        context['user_id'] = account.id
+        context["username"] = account.username
+        context["email"] = account.email
+        context["profile_image"] = account.profile_image.url
+        context["hide_email"] = account.hide_email
+
+        # define template variable for is_self and is_friend
+        is_self = True
+        is_friend = False
+        user = request.user
+        if user.is_authenticated and user != account:
+            is_self = False
+        elif not user.is_authenticated:
+            is_self = False
+        context['is_self'] = is_self
+        context['is_friend'] = is_friend
+        context['BASE_URL'] = settings.BASE_URL
+    
+        return render(request, 'account/account.html', context=context)
+    else:
+        return redirect("login")
+
+# Search friends
+
+def account_search_view(request, *args, **kwargs):
+    context = {}
+    print("view called")
+    if request.method == "GET":
+        search_query = request.GET.get("q")
+        print(search_query)
+        if len(search_query) > 0:
+            search_results = Account.objects.filter(email__icontains=search_query).filter(username__icontains=search_query).distinct()
+            user = request.user
+            accounts = []  # [(account1, True), (account2, False), ...]
+            if user.is_authenticated:
+                # get the authenticated users friend list
+                # auth_user_friend_list = FriendList.objects.get(user=user)
+                for account in search_results:
+                    accounts.append((account, False))  # auth_user_friend_list.is_mutual_friend(account)
+                context['accounts'] = accounts
+            else:
+                for account in search_results:
+                    accounts.append((account, False))
+                context['accounts'] = accounts
+    # return HttpResponse("Data found")            
+    return render(request, "account/search_results.html", context)
