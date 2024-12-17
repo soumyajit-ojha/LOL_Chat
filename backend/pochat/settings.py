892136a10473836293424386085a8a5ec3bbc883
soumyajit-ojha <soumyajitojhag2@gmail.com>
2024-12-17 12:21:30 +0530

    max file size defined.

--- backend/pochat/settings.py
+++ backend/pochat/settings.py
@@ -126,6 +126,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = path.join(BASE_DIR, 'media_cdn')
 
 BASE_URL = "http://127.0.0.1:8000"
+MAX_PHOTO_SIZE = 5242880 # 5 * 1024 * 1025 ( 5 MB)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
