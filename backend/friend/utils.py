af4546a5b7c61dfb236cc63fd0626021393e842d
soumyajit-ojha <soumyajitojhag2@gmail.com>
2025-01-22 11:50:02 +0530

    utils.py added to reduce code complexness and reusability.

--- backend/friend/utils.py
+++ backend/friend/utils.py
@@ -1,8 +1,20 @@
-from friend.models import FriendRequest
-def get_friend_request_or_not(sender,receiver):
+from .models import FriendRequest
 
+
+# Utility to get friend request status
+def get_friend_request_status(sender, receiver):
+    """
+    Returns the status of a friend request between sender and receiver.
+    """
     try:
-        return FriendRequest.objects.get(sender=sender, receiver=receiver, is_active=True)
+        friend_request = FriendRequest.objects.get(sender=sender, receiver=receiver, is_active=True)
+        return friend_request
     except FriendRequest.DoesNotExist:
         return False
-            
+
+def get_friend_request(user, *args, **kwargs):
+    try:
+        all_requests = FriendRequest.objects.filter(receiver=user)
+    except FriendRequest.DoesNotExist:
+        all_requests = None
+    return all_requests
