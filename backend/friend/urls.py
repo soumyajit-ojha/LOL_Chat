72e63e23d8c8243d5392e0330d39aed21a8d0e22
soumyajit-ojha <soumyajitojhag2@gmail.com>
2025-01-23 20:32:11 +0530

    url added to friend request accept.

--- backend/friend/urls.py
+++ backend/friend/urls.py
@@ -4,11 +4,13 @@ from django.conf.urls.static import static
 from .views import (
     SendFriendRequest,
     AllFriendRequest,
+    AcceptFriendRequest,
 )
 
 urlpatterns = [
     path('friend_request/', SendFriendRequest.as_view(), name='friend-request-sent'),
     path('friend_request/<int:user_id>', AllFriendRequest.as_view(), name='friend-request'),
+    path('accept_friend_request/<int:friend_request_id>', AcceptFriendRequest.as_view(), name='friend-request-accept'),
 ]
 
 if settings.DEBUG:
