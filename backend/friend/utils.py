dc5b193958ef54d480e9a063fc1f19af3f3c503f
soumyajit-ojha <soumyajitojhag2@gmail.com>
2025-01-23 20:33:27 +0530

    all friend request filter modified, is_active added.

--- backend/friend/utils.py
+++ backend/friend/utils.py
@@ -14,7 +14,7 @@ def get_friend_request_status(sender, receiver):
 
 def get_friend_request(user, *args, **kwargs):
     try:
-        all_requests = FriendRequest.objects.filter(receiver=user)
+        all_requests = FriendRequest.objects.filter(receiver=user, is_active=True)
     except FriendRequest.DoesNotExist:
         all_requests = None
     return all_requests
