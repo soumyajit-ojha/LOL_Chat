0827917faa336ef39cc97abeb976b26c2da83996
soumyajit-ojha <soumyajitojhag2@gmail.com>
2025-01-27 12:00:29 +0530

    requst decline url created to perform decline requests.

--- backend/friend/urls.py
+++ backend/friend/urls.py
@@ -5,6 +5,7 @@ from .views import (
     SendFriendRequest,
     AllFriendRequest,
     AcceptFriendRequest,
+    DeclineFriendRequest,
     Unfriend,
 )
 
@@ -12,6 +13,7 @@ urlpatterns = [
     path('friend_request_sent/', SendFriendRequest.as_view(), name='friend-request-sent'),
     path('friend_request/<int:user_id>', AllFriendRequest.as_view(), name='friend-request'),
     path('accept_friend_request/<int:friend_request_id>', AcceptFriendRequest.as_view(), name='friend-request-accept'),
+    path('decline_friend_request/<int:friend_request_id>', DeclineFriendRequest.as_view(), name='friend-request-decline'),
     path('unfriend/', Unfriend.as_view(), name='unfriend')
 ]
 
