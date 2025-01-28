8ef2fa84db240f39c500223a6fa9e3dc5528e78f
soumyajit-ojha <soumyajitojhag2@gmail.com>
2025-01-28 09:28:33 +0530

    cancel friend request url created and implemented.

--- backend/friend/urls.py
+++ backend/friend/urls.py
@@ -7,12 +7,14 @@ from .views import (
     AcceptFriendRequest,
     DeclineFriendRequest,
     Unfriend,
+    CancelFriendRequest,
 )
 
 urlpatterns = [
-    path('friend_request_sent/', SendFriendRequest.as_view(), name='friend-request-sent'),
+    path('send_friend_request/', SendFriendRequest.as_view(), name='friend-request-sent'),
     path('friend_request/<int:user_id>', AllFriendRequest.as_view(), name='friend-request'),
     path('accept_friend_request/<int:friend_request_id>', AcceptFriendRequest.as_view(), name='friend-request-accept'),
+    path('cancel_friend_request/', CancelFriendRequest.as_view(), name='friend-request-decline'),
     path('decline_friend_request/<int:friend_request_id>', DeclineFriendRequest.as_view(), name='friend-request-decline'),
     path('unfriend/', Unfriend.as_view(), name='unfriend')
 ]
