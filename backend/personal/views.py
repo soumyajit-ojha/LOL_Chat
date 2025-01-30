d30706a8823c0a4eff04437ebd9946d23194c475
soumyajit-ojha <soumyajitojhag2@gmail.com>
2025-01-30 20:59:51 +0530

    home screen view added.

--- backend/personal/views.py
+++ backend/personal/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from django.views import View
+from friend.models import FriendList
 
-def home_screen_view(request, *args, **kwargs):
-    context = {}
-    return render(request, 'personal/home.html')
+class HomeScreenView(View):
+    def get(self, request, *args, **kwargs):
+        user = request.user
+        context = {}
+
+        if not user.is_authenticated:
+            return render(request, 'personal/home.html',context)
+        
+        try:
+            user_friends = FriendList.objects.get(user=user)
+            context["friends"] = user_friends.friends.all()
+        except FriendList.DoesNotExist:
+            context["friends"] = []
+
+        return render(request, 'personal/home.html',context)
+
+
